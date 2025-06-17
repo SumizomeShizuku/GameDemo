2e9562abbe553960515ad280683359193b364d3f
SumizomeShizuku <98514826+SumizomeShizuku@users.noreply.github.com>
2025-06-18 00:57:01 +0900

    添加版本信息

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,8 +1,13 @@
 package org.demo;
 
+import org.demo.util.SimpleLogger;
+
 public class Main {
 
+    public static final String VERSION = "0.0.1";
+
     public static void main(String[] args) {
+        SimpleLogger.log.info("当前版本: " + VERSION);
         Play.playerInit();
     }
 
