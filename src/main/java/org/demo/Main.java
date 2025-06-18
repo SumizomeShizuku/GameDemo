a1adc316619ff93841f3a8db450cf5a35411e839
SumizomeShizuku <98514826+SumizomeShizuku@users.noreply.github.com>
2025-06-19 02:30:30 +0900

    更新版本号至0.0.2

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -4,7 +4,7 @@ import org.demo.util.SimpleLogger;
 
 public class Main {
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     public static void main(String[] args) {
         SimpleLogger.log.info("当前版本: " + VERSION);
